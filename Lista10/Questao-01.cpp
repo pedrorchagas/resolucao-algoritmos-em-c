ab5f059942bb7cea04306fd4d9202f1886f817c4
pedrorchagas <perodrigues132006@gmail.com>
2025-05-30 13:42:35 -0300

    add: Resposta da questão 01 da lista 10

--- Lista10/Questao-01.cpp
+++ Lista10/Questao-01.cpp
@@ -1,10 +1,28 @@
 #include<stdio.h>
+#include<locale.h>
 
 int main() {
+    setlocale(LC_ALL, "Portuguese");
+    char Reposta;
+    bool Loop = true;
 
     do {
-        
-    } while();
+        printf("Você gosta de ler? [S/N]: ");
+        scanf(" %c", &Reposta);
+        switch (Reposta){
+            case 'n':
+            case 'N':
+            case 'S':
+            case 's':
+                Loop = false;
+                break;
+            default:
+                printf("Resposta inválida, digite S ou N.\n");
+                break;
+        }
+
+    } while(Loop);
+
 
     return 0;
 }
